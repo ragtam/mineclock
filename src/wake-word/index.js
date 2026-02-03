bf8f376e4b648426d04a5c8e9e74db6e6c829208
Mateusz <mateusz.garbaciak.ext@heidelberg.com>
2026-02-03 15:07:14 +0100

    add test word

--- src/wake-word/index.js
+++ src/wake-word/index.js
@@ -3,7 +3,12 @@ import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
 
 export async function registerWakeWord(wordFoundFn) {
   const key = localStorage.getItem('PV_KEY');
-  const porcupine = await PorcupineWorker.create(key, ["Bumblebee"], wordFoundFn, {
+  const keyword = 'Bumblebee';
+  // const keyword = {
+  //   publicPath: `${import.meta.env.BASE_URL}assets/wake-word/keywords/Hot-Pink_en_wasm_v4_0_0.ppn`,
+  //   label: 'Hot Pink'
+  // };
+  const porcupine = await PorcupineWorker.create(key, [keyword], wordFoundFn, {
     publicPath: `${import.meta.env.BASE_URL}assets/wake-word/porcupine_params.pv`,
     customWritePath: "4.0.0_porcupine_params.pv"
   });
